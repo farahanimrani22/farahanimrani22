2727580431f621ec9d61c55f9c3854e2a45490ed
farahanimrani22 <147695863+farahanimrani22@users.noreply.github.com>
2023-10-26 14:53:07 +0800

    Add files via upload

---
+++ EXERCISE 2_U2001614_SIO3016.R
@@ -0,0 +1,12 @@
+#NUR FARAHANIM BINTI AB RANI
+#U2001614/2
+#EXERCISE 2
+
+birthweight <- read.csv("birthweight.csv")
+
+#Are preterm babies more likely to have low birth weight?
+
+#What is the ratio of maternal cigarettes to paternal cigarettes for births at each of the hospitals?
+
+#Do taller mothers have taller partners? Do they have longer babies?
+  
